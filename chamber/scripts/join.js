60ea941ac8c7998c549638cf5d85f102f78885c7
Christoper22rm <christoper22rm@gmail.com>
2026-04-01 21:10:21 -0500

    semana 5

---
+++ chamber/scripts/join.js
@@ -0,0 +1,11 @@
+// timestamp automático
+document.getElementById("timestamp").value = new Date().toISOString();
+
+// MODALS
+function openModal(id) {
+  document.getElementById(id).showModal();
+}
+
+function closeModal(id) {
+  document.getElementById(id).close();
+}
